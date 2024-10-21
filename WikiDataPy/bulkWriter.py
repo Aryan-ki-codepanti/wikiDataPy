be9ecd8a191180828a63a9af48f35e3455be75f5
Aryan Sethi <aryan.rdps@gmail.com>
2024-10-21 23:24:47 +0530

    bulk editing done

--- WikiDataPy/bulkWriter.py
+++ WikiDataPy/bulkWriter.py
@@ -15,11 +15,12 @@ class BulkWriter(WikiWriter):
 
     DELTA = 5
 
-    def addClaimsFromCSV(self, fileSource: str, header=True):
+    def addClaimsFromCSV(self, fileSource: str, header: bool = True):
         """
         Create a new claim on a Wikidata entity.
 
         :param fileSource: str, the path  of the CSV file having data as entity_id, property_id,value_id
+        :param header:  boolean specifying if csv file has header or not
         """
 
         if not self.csrf_token:
@@ -37,6 +38,7 @@ class BulkWriter(WikiWriter):
                 for i in reader:
                     resp.append(self.addClaim(i[0], i[1], i[2]))
                 return resp
+
         except Exception as e:
             print("Error", e)
             return e
@@ -46,14 +48,15 @@ class BulkWriter(WikiWriter):
         Create a new  Wikidata entity per row in CSV file
 
         :param fileSource: str, the path  of the CSV file having
+        :param header:  boolean specifying if csv file has header or not
 
-        CSV file format of rows with optional header but specify if header present
+
+        CSV file format of rows (with optional header but specify if header present)
         language_code,label,description
 
         this creates one entity per row with a single label description at time
         for multiple labels/descriptions in more than one language , create 1 entity then use 'editEntitiesFromCSV' 
-        from its ids
-
+        from entities' ids
         """
 
         if not self.csrf_token:
@@ -71,7 +74,52 @@ class BulkWriter(WikiWriter):
                 for i in reader:
                     resp.append(self.createOrEditEntity(
                         {i[0]: i[1]}, {i[0]: i[2]}))
+
                     sleep(BulkWriter.DELTA)
+                return resp
+
+        except Exception as e:
+            print("Error", e)
+            return e
+        finally:
+            print(
+                "If facing limit issues try after few time or increase BulkWriter.DELTA")
+
+    def editEntitiesFromCSV(self, fileSource: str, header=True):
+        """
+        TODO
+        Performs a edit on Wikidata entity per row in CSV file specified by entity_id
+
+        :param fileSource: str, the path  of the CSV file having
+        :param header:  boolean specifying if csv file has header or not
+
+
+        CSV file format of rows (with optional header but specify if header present) 
+        (can have multiple rows of same entity_id specifying different language label, description)
+        entity_id,language_code,label,description
+
+        for multiple labels/descriptions in more than one language , create 1 entity then use 'editEntitiesFromCSV' 
+        from entities' ids with multiple rows , each row different language
+
+        """
+
+        if not self.csrf_token:
+            print("You have no CSRF token, kindly login and then call getCSRFToken()")
+            return
+
+        try:
+            with open(fileSource, "r") as f:
+                reader = csv.reader(f)
+
+                if header:  # header set
+                    next(reader)
+
+                resp = []
+                for i in reader:
+                    resp.append(self.createOrEditEntity(
+                        {i[1]: i[2]}, {i[1]: i[3]}, i[0]))
+
+                    # sleep(BulkWriter.DELTA)
                 return resp
 
         except Exception as e:
@@ -99,6 +147,15 @@ def bulk_create_entities(w: BulkWriter):
     w.dumpResult(res, f2)
 
 
+def bulk_edit_entities(w: BulkWriter):
+    f1 = "bulk/test3_edit.csv"
+    f2 = "bulk/test_edit_1.json"
+
+    res = w.editEntitiesFromCSV(f1)
+    print("Bulk Edit done")
+    w.dumpResult(res, f2)
+
+
 if __name__ == "__main__":
 
     # bulk add claim test
@@ -109,6 +166,8 @@ if __name__ == "__main__":
 
     # bulk_add_claim_test(w)
 
-    bulk_create_entities(w)
+    # bulk_create_entities(w)
+
+    bulk_edit_entities(w)
 
     w.logout()
