6c593c99402768414558cdfad7e102b437566fc7
Aryan Sethi <aryan.rdps@gmail.com>
2024-10-22 18:51:48 +0530

    added sparql bulk execute and save results

--- WikiDataPy/sparql.py
+++ WikiDataPy/sparql.py
@@ -1,5 +1,9 @@
 import requests
 from BASE import WikiBase
+from datetime import datetime
+import os
+import pprint
+import sys
 
 
 class WikiSparql(WikiBase):
@@ -24,45 +28,95 @@ class WikiSparql(WikiBase):
             print(f"Failed to retrieve data: {response.status_code}")
             return None
 
+    @staticmethod
+    def execute_many(fileSource: str, delimiter="---", output="single", output_dir="sparql_test"):
+
+        try:
+
+            # invalid output set to single
+
+            if output not in ["single", "many"]:
+                output = "single"
+
+            content = ""
+            with open(fileSource) as f:
+                content = f.read()
+            queries = content.split(delimiter)
+
+            result = []
+            for query in queries:
+                x = WikiSparql.execute(query)
+                result.append(x)
+                sys.stdout.flush()
+
+            # create directory if not exist
+            if not os.path.isdir(output_dir):
+                os.mkdir(output_dir)
+
+            t = datetime.now()
+            if output == 'single':
+                WikiSparql.dumpResult(
+                    result, f"{output_dir}/SparQL_Result_{t}.json")
+                print(f"Done Execution, stored results at {
+                      output_dir}/SparQL_Result_{t}.json")
+                return result
+
+            # one file per query
+            for i, x in enumerate(result):
+                WikiSparql.dumpResult(
+                    x, f"{output_dir}/SparQL_Result_{t}_{i+1}.json")
+                sys.stdout.flush()
+
+            print(f"Done execution check {output_dir}")
+            return result
+
+        except Exception as e:
+            print("Error while executing many")
+            return e
+
+
+# all cats
+sparql_query = """
+SELECT ?item ?itemLabel
+WHERE 
+{
+?item wdt:P31 wd:Q146.  # Find entities that are instances of "cat" (Q146)
+SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
+}
+LIMIT 10
+"""
+
+# humans
+humans = """
+SELECT ?human WHERE {
+?human wdt:P31 wd:Q5.  # P31: instance of, Q5: human
+}
+LIMIT 10
+
+"""
+
 
 def test_execute(fname):
-
-    # all cats
-    sparql_query = """
-    SELECT ?item ?itemLabel
-    WHERE 
-    {
-    ?item wdt:P31 wd:Q146.  # Find entities that are instances of "cat" (Q146)
-    SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
-    }
-    LIMIT 10
-    """
-
-    # humans
-    humans = """
-    SELECT ?human WHERE {
-    ?human wdt:P31 wd:Q5.  # P31: instance of, Q5: human
-    }
-    LIMIT 10
-
-    """
 
     res = WikiSparql.execute(humans)
     print("Execute DONE")
     WikiSparql.dumpResult(res, fname)
 
 
+def test_execute_many():
+
+    res = WikiSparql.execute_many(
+        "sparql_test/queries.txt", output="many", output_dir="bulk_sparql")
+    # "sparql_test/queries.txt",  output_dir="bulk_sparql")
+    # "sparql_test/queries.txt")
+    print("Execute Many DONE")
+
+
 if __name__ == "__main__":
 
+    print(datetime.now())
     # test execute
-    test_execute("sparql_test/test2_humans.json")
+    # test_execute("sparql_test/test2_humans.json")
 
-# Execute the query
-# results = query_wikidata_sparql(sparql_query)
-
-# Display the results
-# if results:
-#     for result in results['results']['bindings']:
-#         item = result['item']['value']
-#         label = result['itemLabel']['value']
-#         print(f"Item: {item}, Label: {label}")
+    # test execute many
+    test_execute_many()
